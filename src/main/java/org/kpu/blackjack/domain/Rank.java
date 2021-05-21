6f87c60495193c9f639ee4f5758f8fb737052b83
gowoonsori <gowoonsori97@naver.com>
2021-05-21 17:49:00 +0900

    Add : RankEnum의 String value

--- src/main/java/org/kpu/blackjack/domain/Rank.java
+++ src/main/java/org/kpu/blackjack/domain/Rank.java
@@ -1,15 +1,23 @@
 package org.kpu.blackjack.domain;
 
 public enum Rank {
-	ACE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10);
+	ACE(1,"Ace"), TWO(2,"Two"), THREE(3,"Three"), FOUR(4,"Four"), FIVE(5,"Five"), 
+	SIX(6,"Six"), SEVEN(7,"Seven"), EIGHT(8,"Eight"), NINE(9,"Nine"), TEN(10,"Ten"), JACK(10,"Jack"), QUEEN(10,"Queen"), KING(10,"King");
+	
+	
 
 	private final int cardValue;
+	private final String cardString;
 
-	Rank(int cardValue) {
+	Rank(int cardValue,String str) {
 		this.cardValue = cardValue;
+		this.cardString = str;
 	}
 
 	public int getCardValue() {
 		return cardValue;
+	}
+	public String getCardString() {
+		return cardString;
 	}
 }
